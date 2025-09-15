2dec2efd06d1e8c5c03d3947d11cd2e9712b0ca3
Emre YURTTAS <emre.yurttas@etudiant.univ-reims.fr>
2025-09-15 10:49:46 +0200

    Ajout d'un écouteur d'événement pour install et mise en cache des ressource statiques

---
+++ public/sw.js
@@ -0,0 +1,25 @@
+const STATIC_CACHE_NAME = "todosApp-static.v0";
+
+const addResourcesToCache = async (resources) => {
+  const cache = await caches.open(STATIC_CACHE_NAME);
+  await cache.addAll(resources);
+};
+
+self.addEventListener("install", (event) => {
+  console.log("[Service Worker] Installation en cours");
+  event.waitUntil(
+    addResourcesToCache([
+      "/",
+      "/index.html",
+      "/css/style.css",
+      "/src/main.jsx",
+      "/icons/favicon.ico",
+      "/icons/icon-512x512.png",
+      "/icons/apple-touch-icon-180x180.png",
+      "/icons/pwa-192x192.png",
+      "/icons/pwa-512x512.png",
+      "/icons/pwa-64x64.png"
+    ])
+  );
+  self.skipWaiting();
+});
