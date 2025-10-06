f6d304dd58ae05cc2431c867f23f4fa89aa78517
Emre YURTTAS <emre.yurttas@etudiant.univ-reims.fr>
2025-10-06 10:42:30 +0200

    Suppression la réponse de type cache first

--- public/sw.js
+++ public/sw.js
@@ -53,37 +53,6 @@ self.addEventListener("fetch", (event) => {
     }
 });
 
-const putInCache = async (request, response) => {
-    const cache = await caches.open('todosApp-static');
-    await cache.put(request, response);
-};
-
-const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
-    const responseFromCache = await caches.match(request);
-    if (responseFromCache) return responseFromCache;
-
-    const preloadResponse = await preloadResponsePromise;
-    if (preloadResponse) {
-        putInCache(request, preloadResponse.clone());
-        return preloadResponse;
-    }
-
-    try {
-        const responseFromNetwork = await fetch(request);
-        putInCache(request, responseFromNetwork.clone());
-        return responseFromNetwork;
-    } catch (error) {
-        const fallbackResponse = await caches.match(fallbackUrl);
-        if (fallbackResponse) {
-            return fallbackResponse;
-        }
-        return new Response("Network error happened", {
-            status: 408,
-            headers: { "Content-Type": "text/plain" },
-        });
-    }
-};
-
 self.addEventListener("fetch", (event) => {
     if (event.request.url.includes("background.css")) {
         event.respondWith(
@@ -93,14 +62,5 @@ self.addEventListener("fetch", (event) => {
                 });
             })
         );
-        return;
     }
-
-    event.respondWith(
-        cacheFirst({
-            request: event.request,
-            preloadResponsePromise: event.preloadResponse,
-            fallbackUrl: "/screenshots/screenshot1.png",
-        })
-    );
 });
