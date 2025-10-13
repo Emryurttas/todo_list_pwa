3ce3359d5cd4ce51419d1def7c7238c384ffa673
Emre YURTTAS <emre.yurttas@etudiant.univ-reims.fr>
2025-10-13 11:23:15 +0200

    Ajout du composant mise a jour et utilisation dans main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,27 @@
 import TodoList from "./TodoList";
 import FormAddTodo from "./FormAddTodo.jsx";
 import useTodos from "../hooks/useTodos.js";
-import {InstallPWAButton} from "./InstallPWAButton.jsx";
+import { InstallPWAButton } from "./InstallPWAButton.jsx";
+import { SWUpdateBanner } from "./SWUpdateBanner.jsx";
 
 export default function Main() {
-  const { todos, isLoading, toggleTodo, deleteTodo, updateTodo, addTodo } = useTodos();
+    const { todos, isLoading, toggleTodo, deleteTodo, updateTodo, addTodo } = useTodos();
 
-  return (
-        <main className="main">
-            <InstallPWAButton></InstallPWAButton>
+    return (
+        <main className="main" style={{ minHeight: "100vh", padding: "20px" }}>
+            <SWUpdateBanner />
+
+            <InstallPWAButton />
+
             <FormAddTodo addTodo={addTodo} />
-            <TodoList todos={todos} isLoading={isLoading} toggleTodo={toggleTodo} deleteTodo={deleteTodo} updateTodo={updateTodo} />
+
+            <TodoList
+                todos={todos}
+                isLoading={isLoading}
+                toggleTodo={toggleTodo}
+                deleteTodo={deleteTodo}
+                updateTodo={updateTodo}
+            />
         </main>
-    )
+    );
 }
