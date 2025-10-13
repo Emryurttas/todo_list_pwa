48e6a8884791cf038cd23034a17948afac3a06b1
Emre YURTTAS <emre.yurttas@etudiant.univ-reims.fr>
2025-10-13 10:01:42 +0200

    gérer background.css avec Workbox

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,5 @@
-import { precacheAndRoute, cleanupOutdatedCaches } from 'workbox-precaching';
+import {cleanupOutdatedCaches, precacheAndRoute} from 'workbox-precaching';
+import {registerRoute} from 'workbox-routing';
 
 const TODOS_CACHE_NAME = "todos";
 
@@ -7,7 +8,6 @@ cleanupOutdatedCaches();
 const filteredManifest = self.__WB_MANIFEST.filter(
     entry => !entry.url.includes("css/background.css")
 );
-
 precacheAndRoute(filteredManifest);
 
 const enableNavigationPreload = async () => {
@@ -34,10 +34,8 @@ self.addEventListener("fetch", (event) => {
             (async () => {
                 try {
                     const responseFromNetwork = await fetch(request);
-
                     const todosCache = await caches.open(TODOS_CACHE_NAME);
                     todosCache.put(request, responseFromNetwork.clone());
-
                     console.log("[Service Worker] Liste des todos mise à jour depuis le réseau");
                     return responseFromNetwork;
                 } catch (error) {
@@ -57,14 +55,16 @@ self.addEventListener("fetch", (event) => {
     }
 });
 
-self.addEventListener("fetch", (event) => {
-    if (event.request.url.includes("background.css")) {
-        event.respondWith(
-            fetch(event.request).catch(() => {
-                return new Response(".main {background: orange;}", {
-                    headers: { "Content-Type": "text/css" },
-                });
-            })
-        );
+registerRoute(
+    ({ request, url }) => request.destination === 'style' && url.pathname.endsWith('background.css'),
+    async ({ request }) => {
+        try {
+            return await fetch(request);
+        } catch (error) {
+            console.log("[Service Worker] background.css non disponible");
+            return new Response(".main {background: orange;}", {
+                headers: { "Content-Type": "text/css" },
+            });
+        }
     }
-});
+);
