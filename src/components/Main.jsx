1f7f8e220ee0f2f3a22c6de5e83170a430bd833b
Emre YURTTAS <emre.yurttas@etudiant.univ-reims.fr>
2025-10-13 11:46:46 +0200

    Ajout variable d'etat networkError + section d'indication

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,13 +5,29 @@ import { InstallPWAButton } from "./InstallPWAButton.jsx";
 import { SWUpdateBanner } from "./SWUpdateBanner.jsx";
 
 export default function Main() {
-    const { todos, isLoading, toggleTodo, deleteTodo, updateTodo, addTodo } = useTodos();
+    const { todos, isLoading, toggleTodo, deleteTodo, updateTodo, addTodo, networkError } = useTodos();
 
     return (
         <main className="main" style={{ minHeight: "100vh", padding: "20px" }}>
             <SWUpdateBanner />
 
             <InstallPWAButton />
+
+            {networkError && (
+                <div
+                    style={{
+                        backgroundColor: "#ffcccc",
+                        color: "#990000",
+                        padding: "10px",
+                        borderRadius: "5px",
+                        marginBottom: "15px",
+                        textAlign: "center",
+                        fontWeight: "bold",
+                    }}
+                >
+                    Vous êtes actuellement hors ligne ou le serveur est inaccessible.
+                </div>
+            )}
 
             <FormAddTodo addTodo={addTodo} />
 
--- src/hooks/useTodos.js
+++ src/hooks/useTodos.js
@@ -4,82 +4,78 @@ import {addApiTodo, deleteApiTodo, getApiTodos, updateApiTodo} from "../services
 export default function useTodos() {
     const [todos, setTodos] = useState([]);
     const [isLoading, setLoading] = useState(true);
+    const [networkError, setNetworkError] = useState(false);
 
     useEffect(() => {
-        getAllTodos()
-            .catch((err) => {
-                /* Network error */
-                console.log(err.message);
-            });
+        getAllTodos().catch((err) => {
+            console.log(err.message);
+            setNetworkError(true);
+        });
     }, []);
 
-    const getAllTodos = async function() {
-        setTodos(await getApiTodos());
-        setLoading(false);
-    }
+    const getAllTodos = async function () {
+        try {
+            const data = await getApiTodos();
+            setTodos(data);
+            setNetworkError(false);
+        } catch (err) {
+            console.log(err.message);
+            setNetworkError(true);
+        } finally {
+            setLoading(false);
+        }
+    };
 
     const getTodos = () => {
-        console.log("get Todos");
-
-        getAllTodos()
-          .catch((err) => {
-              /* Network error */
-              console.log(err.message);
-          });
+        getAllTodos();
     };
 
     const toggleTodo = (id) => {
-        console.log("toggleTodo Todo :", id);
-
         const todoToUpdate = todos.find((todo) => todo.id === id);
-        updateApiTodo({...todoToUpdate, done: !todoToUpdate.done})
-          .then(() => getAllTodos())
-          .catch((err) => {
-              /* Network error */
-              console.log(err.message);
-          });
+        updateApiTodo({ ...todoToUpdate, done: !todoToUpdate.done })
+            .then(() => getAllTodos())
+            .catch((err) => {
+                console.log(err.message);
+                setNetworkError(true);
+            });
     };
 
     const deleteTodo = (id) => {
-        console.log("delete Todo :", id);
-
         deleteApiTodo(id)
-          .then(() => getAllTodos())
-          .catch((err) => {
-              /* Network error */
-              console.log(err.message);
-          });
+            .then(() => getAllTodos())
+            .catch((err) => {
+                console.log(err.message);
+                setNetworkError(true);
+            });
     };
 
     const updateTodo = (id, text) => {
-        console.log("update Todo :", id, text);
         const todoToUpdate = todos.find((todo) => todo.id === id);
-        updateApiTodo({...todoToUpdate, text})
-          .then(() => getAllTodos())
-          .catch((err) => {
-              /* Network error */
-              console.log(err.message);
-          });
+        updateApiTodo({ ...todoToUpdate, text })
+            .then(() => getAllTodos())
+            .catch((err) => {
+                console.log(err.message);
+                setNetworkError(true);
+            });
     };
 
     const addTodo = (text) => {
-        console.log("add Todo :", text);
-
         addApiTodo(text)
-          .then(() => getAllTodos())
-          .catch((err) => {
-              /* Network error */
-              console.log(err.message);
-          });
+            .then(() => getAllTodos())
+            .catch((err) => {
+                console.log(err.message);
+                setNetworkError(true);
+            });
     };
 
     return {
         todos,
         isLoading,
+        networkError,
         toggleTodo,
         addTodo,
         deleteTodo,
         updateTodo,
         getTodos,
-    }
+    };
 }
