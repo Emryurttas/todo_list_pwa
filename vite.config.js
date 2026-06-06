3b039b93f5f51883ce4b58a2fac45f9788d5d7a2
Emryurttas <yurttasemre566@gmail.com>
2026-06-06 14:44:48 +0200

    config 2

--- vite.config.js
+++ vite.config.js
@@ -3,48 +3,71 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/todo-list-pwa/',
-
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/app.js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]'
-      }
-    }
-  },
+  base: "/todo-list-pwa/",
 
   plugins: [
     react(),
 
     VitePWA({
-      registerType: 'autoUpdate',
-      injectRegister: 'auto',
-
       strategies: 'injectManifest',
       srcDir: 'public',
       filename: 'sw.js',
+      registerType: 'autoUpdate',
+
+      includeAssets: [
+        'icons/*.png',
+        'screenshots/*.png',
+        'css/*.css',
+      ],
 
       manifest: {
-        id: '/todo-list-pwa/',
-        name: 'todolist',
         short_name: 'list',
+        name: 'todolist',
 
+        id: '/todo-list-pwa/?source=pwa',
         start_url: '/todo-list-pwa/?source=pwa',
         scope: '/todo-list-pwa/',
 
         display: 'standalone',
         background_color: '#3367D6',
         theme_color: '#3367D6',
+        description: 'Application de gestion de tâches en PWA',
+
+        screenshots: [
+          {
+            src: 'screenshots/screenshot1.png',
+            sizes: '1342x786',
+            type: 'image/png',
+            form_factor: 'wide'
+          }
+        ],
 
         icons: [
           {
+            src: 'icons/apple-touch-icon-180x180.png',
+            type: 'image/png',
+            sizes: '180x180'
+          },
+          {
+            src: 'icons/favicon.ico',
+            type: 'image/x-icon',
+            sizes: '48x48'
+          },
+          {
+            src: 'icons/maskable-icon-512x512.png',
+            type: 'image/png',
+            sizes: '512x512',
+            purpose: 'maskable'
+          },
+          {
             src: 'icons/pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
+            type: 'image/png',
+            sizes: '192x192'
+          },
+          {
+            src: 'icons/pwa-512x512.png',
+            type: 'image/png',
+            sizes: '512x512'
           }
         ]
       }
