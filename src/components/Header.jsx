455a18dbca4e034de3ed6d96926e91c14675d3df
Emryurttas <yurttasemre566@gmail.com>
2026-06-06 19:10:53 +0200

    logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
     return (
         <header>
             <h1>My Wonderful Todo List</h1>
-            <img src="/icons/icon-512x512.png" alt="logo" width="64" height="64"/>
+            <img src="/todo_list_pwa/icons/pwa-512x512.png" alt="logo" width="64" height="64"/>
         </header>
     )
 }
