eb7c9ad38a8aafe5f43ba295f6769673c8204a40
Emryurttas <yurttasemre566@gmail.com>
2026-06-06 19:33:30 +0200

    delete logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,6 @@ export default function Header() {
     return (
         <header>
             <h1>My Wonderful Todo List</h1>
-            <img src="/todo_list_pwa/public/icons/icon-512x512.png" alt="logo" width="64" height="64"/>
         </header>
     )
 }
