c99b823b8bb08f390dea9db4444b163ca9aeebe7
Emryurttas <yurttasemre566@gmail.com>
2026-06-18 02:35:50 +0200

    icon add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ export default function Header() {
     return (
         <header>
             <h1>My Wonderful Todo List</h1>
+            <img src="/icons/icon-512x512.png" alt="logo" width="64" height="64"/>
         </header>
     )
 }
